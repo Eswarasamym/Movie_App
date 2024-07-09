3ea779f164e4d4f554b89eba34ac04b4939310fb
eswarasamy <eswarasamy87@gmail.com>
2024-07-09 10:10:58 +0530

    file updted

---
+++ script.js
@@ -0,0 +1,31 @@
+const movies = [
+  "Naruto",
+  "Movie Title 2",
+  // Add more movie titles here
+];
+
+const searchBar = document.getElementById("search-bar");
+const autocompleteList = document.getElementById("autocomplete-list");
+
+searchBar.addEventListener("input", function () {
+  const value = this.value;
+  autocompleteList.innerHTML = "";
+  if (!value) {
+    return false;
+  }
+  movies.forEach((movie) => {
+    if (movie.toLowerCase().includes(value.toLowerCase())) {
+      const item = document.createElement("div");
+      item.classList.add("autocomplete-item");
+      item.innerHTML = `<strong>${movie.substr(
+        0,
+        value.length
+      )}</strong>${movie.substr(value.length)}`;
+      item.addEventListener("click", function () {
+        searchBar.value = movie;
+        autocompleteList.innerHTML = "";
+      });
+      autocompleteList.appendChild(item);
+    }
+  });
+});
